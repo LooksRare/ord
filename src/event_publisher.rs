8a945ab958a1a372629615a0f2135e1043559e4c
rob <rob@looksrare.org>
2024-05-04 10:36:27 +0200

    Move event publisher to new connection way

--- src/event_publisher.rs
+++ src/event_publisher.rs
@@ -1,5 +1,7 @@
 use anyhow::{Context, Result};
 use lapin::options::ConfirmSelectOptions;
+use lapin::tcp::{AMQPUriTcpExt, RustlsConnector};
+use lapin::uri::AMQPUri;
 use lapin::{options::BasicPublishOptions, BasicProperties, Connection, ConnectionProperties};
 use tokio::runtime::Runtime;
 use tokio::sync::mpsc;
@@ -27,7 +29,17 @@ impl EventPublisher {
 
     std::thread::spawn(move || {
       Runtime::new().expect("runtime is setup").block_on(async {
-        let conn = Connection::connect(&addr, ConnectionProperties::default())
+        let uri = addr.parse::<AMQPUri>().unwrap();
+
+        let connect = move |uri: &AMQPUri| {
+          let conn = uri.connect().and_then(|stream| {
+            let connector = RustlsConnector::new_with_native_certs().unwrap();
+            stream.into_rustls(&connector, &uri.authority.host)
+          });
+          conn
+        };
+
+        let conn = Connection::connector(uri, Box::new(connect), ConnectionProperties::default())
           .await
           .expect("connects to rabbitmq ok");
 
